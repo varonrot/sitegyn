e8e69a8296b2faf31f6fafb4bbde94ea86e6c67e
varonrot <varonrot@gmail.com>
2025-11-17 08:46:44 +0200

    Update templates_config.py

--- config/templates_config.py
+++ config/templates_config.py
@@ -1,8 +1,8 @@
+# config/templates_config.py
+
 TEMPLATES = {
-    "pizza": [
-        "template_pizza_01"
-    ]
-}
-DEFAULT_TEMPLATE_BY_NICHE = {
-    "pizza": "template_pizza_01"
+    "template_pizza_01": {
+        "html": "templates/template_pizza_01/template_pizza_01.html",
+        "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json"
+    }
 }
