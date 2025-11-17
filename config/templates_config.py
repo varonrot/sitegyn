52146edc10c9ba28d6f19be562fafcee69bc9434
varonrot <varonrot@gmail.com>
2025-11-17 13:19:02 +0200

    Update templates_config.py

--- config/templates_config.py
+++ config/templates_config.py
@@ -1,8 +1,10 @@
-# config/templates_config.py
-
 TEMPLATES = {
     "template_pizza_01": {
         "html": "templates/template_pizza_01/template_pizza_01.html",
-        "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json"
+        "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json",
+    },
+    "template_pizza_02": {
+        "html": "templates/template_pizza_02/template_pizza_02.html",
+        "mapping": "templates/template_pizza_02/template_pizza_02_mapping.json",
     }
 }
