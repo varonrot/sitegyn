26aef29fca5f2761478603defd6718a0452a5b14
varonrot <varonrot@gmail.com>
2025-11-18 06:01:44 +0200

    Update templates_config.py

--- config/templates_config.py
+++ config/templates_config.py
@@ -1,4 +1,9 @@
+# config/templates_config.py
+
 TEMPLATES = {
+    # =====================
+    # PIZZA
+    # =====================
     "template_pizza_01": {
         "html": "templates/template_pizza_01/template_pizza_01.html",
         "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json",
@@ -6,5 +11,21 @@ TEMPLATES = {
     "template_pizza_02": {
         "html": "templates/template_pizza_02/template_pizza_02.html",
         "mapping": "templates/template_pizza_02/template_pizza_02_mapping.json",
-    }
+    },
+
+    # =====================
+    # HAIR SALON
+    # =====================
+    "template_hair_salon_01": {
+        "html": "templates/template_hair_salon_01/template_hair_salon_01.html",
+        "mapping": "templates/template_hair_salon_01/template_hair_salon_01_mapping.json",
+    },
+
+    # =====================
+    # PHOTOGRAPHY
+    # =====================
+    "template_photography_01": {
+        "html": "templates/template_photography_01/template_photography_01.html",
+        "mapping": "templates/template_photography_01/template_photography_01_mapping.json",
+    },
 }
