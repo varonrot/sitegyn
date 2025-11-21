a4b569a99caed842dedd960566a3b0a1cc8bd368
varonrot <varonrot@gmail.com>
2025-11-21 16:59:53 +0200

    Update templates_config.py

--- templates_config.py
+++ templates_config.py
@@ -1,7 +1,7 @@
 TEMPLATES = {
     "template_pizza_01": {
-        "html": "templates/template_pizza_01/template_pizza_01.html",
-        "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json",
-        "schema": "templates/template_pizza_01/template_pizza_01_schema.json"
+        "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
+        "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
+        "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
     }
 }
