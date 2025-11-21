21cdb2a3d0df7b19d302679e92408f44047f6cf5
varonrot <varonrot@gmail.com>
2025-11-21 14:41:45 +0200

    Delete config directory

--- config/templates_config.py
+++
@@ -1,8 +0,0 @@
-TEMPLATES = {
-    "template_pizza_01": {
-          "html": "templates/template_pizza_01/template_pizza_01.html",
-          "mapping": "templates/template_pizza_01/template_pizza_01_mapping.json",
-          "schema": "templates/template_pizza_01/template_pizza_01_schema.json"
-
-    }
-}
