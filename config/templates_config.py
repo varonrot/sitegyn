7b90db8701988a66ac9c94dead746047d09cd9c7
varonrot <varonrot@gmail.com>
2025-11-21 13:02:02 +0200

    Update templates_config.py

--- config/templates_config.py
+++ config/templates_config.py
@@ -1,11 +1,6 @@
 TEMPLATES = {
-    "pizza": {
-        "templates": {
-            "template_pizza_01": {
-                "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
-                "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
-                "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
-            }
-        }
+    "template_pizza_01": {
+        "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
+        "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
+        "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
     }
-}
