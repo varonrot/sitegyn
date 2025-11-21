be49b28dc212106218a9a533b348b6857b273355
varonrot <varonrot@gmail.com>
2025-11-21 12:09:30 +0200

    Update templates_config.py

--- config/templates_config.py
+++ config/templates_config.py
@@ -7,9 +7,7 @@ TEMPLATES = {
     "template_pizza_01": {
         "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
         "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
-        # נשתמש בפרומפט הכללי שנמצא ב-root: content_fill_prompt.txt
-        # ולכן לא צריך content_prompt מותאם אישית כרגע
-        # "content_prompt": "content_fill_prompt.txt",
         "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
+
     },
 }
