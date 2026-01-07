51a1e7590ac34f2258d66b6e02c8869b1a1135ba
varonrot <varonrot@gmail.com>
2026-01-07 10:23:44 +0200

    Update templates_config.py

--- templates_config.py
+++ templates_config.py
@@ -1,7 +1,13 @@
 TEMPLATES = {
-    "template_pizza_01": {
-        "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
-        "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
-        "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
-    }
+  "template_pizza_01": {
+    "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
+    "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
+    "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json"
+  },
+
+  "template_lawyer_01": {
+    "html": "sitegyn/templates/template_lawyer_01/template_lawyer_01.html",
+    "mapping": "sitegyn/templates/template_lawyer_01/template_lawyer_01_mapping.json",
+    "schema": "sitegyn/templates/template_lawyer_01/template_lawyer_01_schema.json"
+  }
 }
