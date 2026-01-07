1283554863156e9794afa807af67bfd0b135cdb5
varonrot <varonrot@gmail.com>
2026-01-07 10:23:25 +0200

    Update templates_config_back_up.py

--- templates_config_back_up.py
+++ templates_config_back_up.py
@@ -1,1 +1,7 @@
-
+TEMPLATES = {
+    "template_pizza_01": {
+        "html": "sitegyn/templates/template_pizza_01/template_pizza_01.html",
+        "mapping": "sitegyn/templates/template_pizza_01/template_pizza_01_mapping.json",
+        "schema": "sitegyn/templates/template_pizza_01/template_pizza_01_schema.json",
+    }
+}
