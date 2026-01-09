abf7bbf90b55b88e160f5f2ce7018657f3c0d032
varonrot <varonrot@gmail.com>
2026-01-09 04:57:27 +0200

    Update templates_config.py

--- templates_config.py
+++ templates_config.py
@@ -9,5 +9,11 @@ TEMPLATES = {
     "html": "sitegyn/templates/template_lawyer_01/template_lawyer_01.html",
     "mapping": "sitegyn/templates/template_lawyer_01/template_lawyer_01_mapping.json",
     "schema": "sitegyn/templates/template_lawyer_01/template_lawyer_01_schema.json"
+  },
+
+  "template_online_course_01": {
+    "html": "sitegyn/templates/template_online_course_01/template_online_course_01.html",
+    "mapping": "sitegyn/templates/template_online_course_01/template_online_course_01_mapping.json",
+    "schema": "sitegyn/templates/template_online_course_01/template_online_course_01_schema.json"
   }
 }
